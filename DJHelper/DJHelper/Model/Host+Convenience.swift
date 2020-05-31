aff27d752b24aa17155f5f5559e7b627b634bb50
Michael <mrflowers22@uwalumni.com>
2020-05-31 10:30:31 -0700

    created a convenience initializer function for the Host that sets the default context to our core data stack's context.

--- DJHelper/DJHelper/Model/Host+Convenience.swift
+++ DJHelper/DJHelper/Model/Host+Convenience.swift
@@ -22,4 +22,18 @@ extension Host {
         case profilePic = "profile_pic_url"
         case identifier = "id"
     }
+    
+    convenience init (name: String, username: String, email: String, password: String, bio: String, identifier: Int32, phone: String, profilePic: URL, website: URL, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.name = name
+        self.username = username
+        self.email = email
+        self.password = password
+        self.bio = bio
+        self.identifier = identifier
+        self.phone = phone
+        self.profilePic = profilePic
+        self.website = website
+    }
+    
 }
