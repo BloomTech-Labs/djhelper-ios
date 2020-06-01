0bde686e8292078aac96ca17a19c95013e9dc116
Michael <mrflowers22@uwalumni.com>
2020-06-01 11:00:55 -0700

    created a hostLogin struct to get login information from server. created a hostLoginResponse struct to handle and decode the data we get back from the endpoint created a computed property in the Host+Convenience model so that we can initialize a hostLogin from the Host's properties username and password.

--- DJHelper/DJHelper/Model/HostController.swift
+++ DJHelper/DJHelper/Model/HostController.swift
@@ -24,11 +24,11 @@ class HostController {
     var bearer: Bearer?
     
     let dataLoader: NetworkDataLoader
-
+    
     init(dataLoader: NetworkDataLoader = URLSession.shared) {
         self.dataLoader = dataLoader
     }
-
+    
     // MARK: - Register New Host
     // the server returns the host properties along with the generated ID
     func registerHost(with host: Host, completion: @escaping (Result<HostRegistrationResponse, HostErrors>) -> Void) {
@@ -57,12 +57,12 @@ class HostController {
             if let response = response as? HTTPURLResponse {
                 print("HTTPResponse: \(response.statusCode) in function: \(#function)")
             }
-
+            
             if let error = error {
                 print("Error: \(error.localizedDescription) on line \(#line) in function: \(#function)\n Technical error: \(error)")
                 completion(.failure(.registrationError(error)))
             }
-//
+            //
             guard let data = data else {
                 print("Error on line: \(#line) in function: \(#function)")
                 completion(.failure(.noDataError))
@@ -72,24 +72,70 @@ class HostController {
             do {
                 let hostRegistrationResponse = try JSONDecoder().decode(HostRegistrationResponse.self, from: data)
                 //we cannot initalize a host because the response doesn't bring back a password, which we need for the host, unless we make a password optional, which I don't think makes sense. There should be a password. So we have to complete with the response
-
+                
                 completion(.success(hostRegistrationResponse))
             } catch {
-                 print("Error on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
+                print("Error on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
                 completion(.failure(.unknownError(error)))
             }
         }
     }
-
+    
     // MARK: - Log In Existing Host
     // the server returns the host properties along with the generated bearer token
-    func logIn(with host: Host, completion: @escaping (Error?) -> Void) {
-
+    func logIn(with host: Host, completion: @escaping (Result<HostLoginResponse, HostErrors>) -> Void) {
+        //take the host and turn in into a hr
+        guard let hostRegistration = host.hostRepRegistration else { return }
+        
+        //create url
+        let registrationURL = baseURL.appendingPathComponent("register").appendingPathComponent("dj")
+        
+        //create url request method
+        var urlRequest = URLRequest(url: registrationURL)
+        urlRequest.httpMethod = HTTPMethod.post.rawValue
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        //pass data into httpBody
+        do {
+            urlRequest.httpBody = try JSONEncoder().encode(hostRegistration)
+        } catch  {
+            print("Error encoding HostRepresentation on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
+        }
+        
+        print("This is the url for posting: \(registrationURL.absoluteString)")
+        
+        //urlsession.shared.dataTask
+        dataLoader.loadData(from: urlRequest) { (data, response, error) in
+            //the response is coming from loadData, not the server per se. The loadDate passes it to us if the response is anything other than 201. if it is 201 then itll skip this. Same for error and data.
+            if let response = response as? HTTPURLResponse {
+                print("HTTPResponse: \(response.statusCode) in function: \(#function)")
+            }
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription) on line \(#line) in function: \(#function)\n Technical error: \(error)")
+                completion(.failure(.registrationError(error)))
+            }
+            //
+            guard let data = data else {
+                print("Error on line: \(#line) in function: \(#function)")
+                completion(.failure(.noDataError))
+                return
+            }
+            
+            do {
+                let hostRegistrationResponse = try JSONDecoder().decode(HostRegistrationResponse.self, from: data)
+                //we cannot initalize a host because the response doesn't bring back a password, which we need for the host, unless we make a password optional, which I don't think makes sense. There should be a password. So we have to complete with the response
+                
+                completion(.success(hostRegistrationResponse))
+            } catch {
+                print("Error on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
+                completion(.failure(.unknownError(error)))
+            }
+        }
     }
-
+    
     // MARK: - Update Existing Host
     // server does not presently have a PUT update for host(DJ)
-
+    
     // MARK: - Delete Host
     // server does not presently have a DEL for host(DJ)
 }
