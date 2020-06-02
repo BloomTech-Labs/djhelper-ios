2ccb56ff5678f1a7ca0e3d45594a38d957c9604c
Michael <mrflowers22@uwalumni.com>
2020-06-01 19:27:56 -0700

    pushing up to branch so that Craig can have at it

--- DJHelper/DJHelper/View Controllers/ViewController.swift
+++ DJHelper/DJHelper/View Controllers/ViewController.swift
@@ -13,19 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let hc = HostController()
-        let host = Host(name: "test18 ", username: "test18", email: "test18", password: "test18", bio: "test18", identifier: 1, phone: "test18", profilePic: URL(string: "test18")!, website: URL(string: "test18")!)
-        //        hc.registerHost(with: host) { (result) in
-        //            switch result {
-        //            case .success(let host): print("successful host: \(String(describing: host.name))")
-        //            case .failure(let error): print("Error on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
-        //            }
-        //        }
-        hc.logIn(with: host) { (result) in
-            switch result {
-            case .success(let host): print("successful host: \(String(describing: host.name))")
+//        let hc = HostController()
+//        let host = Host(name: "test18 ", username: "test18", email: "test18", password: "test18", bio: "test18", identifier: 1, phone: "test18", profilePic: URL(string: "test18")!, website: URL(string: "test18")!)
+//        //        hc.registerHost(with: host) { (result) in
+//        //            switch result {
+//        //            case .success(let host): print("successful host: \(String(describing: host.name))")
+//        //            case .failure(let error): print("Error on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
+//        //            }
+//        //        }
+//        hc.logIn(with: host) { (result) in
+//            switch result {
+//            case .success(let host): print("successful host: \(String(describing: host.name))")
+//            case .failure(let error): print("Error on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
+//            }
+        
+        let ec = EventController()
+        let date = Date()
+        
+        let event = Event(name: "e2", eventType: "e2", eventDescription:"e2" , eventDate: date, hostID: 1, locationID: 1, startTime: date, endTime: Date().advanced(by: 888904), imageURL: URL(string: "e2")!, notes: "e2", eventID: 1)
+        
+        ec.authorize(event: event) { (results) in
+            switch results {
+            case .success(let er): print("this is the event name: \(er.name)")
             case .failure(let error): print("Error on line: \(#line) in function: \(#function)\n Readable error: \(error.localizedDescription)\n Technical error: \(error)")
             }
         }
-    }
+        
+        }
+    
 }
