09e3944ee953d6f55c83c392f64956eabd932f73
Michael <mrflowers22@uwalumni.com>
2020-06-03 10:35:16 -0700

    created an CreateEventViewController file

---
+++ DJHelper/DJHelper/View Controllers/CreateEventViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateEventViewController.swift
+//  DJHelper
+//
+//  Created by Michael Flowers on 6/3/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
