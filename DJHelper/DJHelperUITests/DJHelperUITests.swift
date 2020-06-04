5dd9330d19bc4ee74ce9bbcc90f09e5f73c489ac
Craig Swanson <cswanson02@me.com>
2020-06-04 13:46:14 -0500

    Created three UI tests.

--- DJHelper/DJHelperUITests/DJHelperUITests.swift
+++ DJHelper/DJHelperUITests/DJHelperUITests.swift
@@ -10,14 +10,83 @@ import XCTest
 
 class DJHelperUITests: XCTestCase {
     
+    // Register with different password fields should present password alert
+    // Register with empty text field should return nothing
+    // Register with a unique username and matching passwords should present success alert
+    // ...and continuing to sign in should present table view controller.
+
     let app = XCUIApplication()
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    override func setUp() {
+        continueAfterFailure = false
         app.launch()
+    }
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInvalidLogIn() {
+
+        // TODO: This test helped me to find an error!
+        // We need to add an alert if the fetch request does not find the username in core data.
+
+        let usernameTextField = app.textFields["HostSignIn.UsernameTextField"]
+            usernameTextField.tap()
+        usernameTextField.typeText("FakeUsername")
+
+        let hostsigninPasswordtextfieldTextField = app.textFields["HostSignIn.PasswordTextField"]
+        hostsigninPasswordtextfieldTextField.tap()
+        hostsigninPasswordtextfieldTextField.typeText("password")
+        app.staticTexts["Sign in"].tap()
+
+        addUIInterruptionMonitor(withDescription: "System Dialog") { (alert) -> Bool in
+            let invalidSignInText = "LogIn Error"
+            if alert.label.contains(invalidSignInText) {
+                XCTAssertTrue(alert.exists)
+                alert.buttons["OK"].tap()
+            } else {
+                XCTFail()  // Want it to fail if it reaches this case
+            }
+            return true
+        }
+        app.tap()
+    }
+
+    func testShowRegisterScene() {
+
+        app.staticTexts["Register"].tap()
+        XCTAssertTrue(app.staticTexts["Create your Account"].exists)
+    }
+
+    func testRegisterUnmatchedPasswords() {
+
+        app.staticTexts["Register"].tap()
+
+        let usernameTextField = app.textFields["HostRegistration.UsernameTextField"]
+            usernameTextField.tap()
+        usernameTextField.typeText("plastic")
+
+        let emailTextField = app.textFields["HostRegistration.EmailTextField"]
+            emailTextField.tap()
+        emailTextField.typeText("email@me.com")
+
+        let passwordTextField = app.textFields["HostRegistration.PasswordTextField"]
+            passwordTextField.tap()
+        passwordTextField.typeText("first")
+
+        let confirmTextField = app.textFields["HostRegistration.ConfirmTextField"]
+            confirmTextField.tap()
+        confirmTextField.typeText("second")
+
+        app.staticTexts["Create Account"].tap()
+
+        addUIInterruptionMonitor(withDescription: "System Dialog") { (alert) -> Bool in
+            let invalidPasswordText = "Password Error"
+            if alert.label.contains(invalidPasswordText) {
+                XCTAssertTrue(alert.exists)
+                alert.buttons["OK"].tap()
+            } else {
+                XCTFail()  // Want it to fail if it reaches this case
+            }
+            return true
+        }
+        app.tap()
+
     }
 }
