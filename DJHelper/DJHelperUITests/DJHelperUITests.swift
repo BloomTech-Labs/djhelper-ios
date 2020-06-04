ae110745118df00c4e9b38cf4bf510cd12c0b80f
Craig Swanson <cswanson02@me.com>
2020-06-03 22:49:36 -0500

    final before a PR.

--- DJHelper/DJHelperUITests/DJHelperUITests.swift
+++ DJHelper/DJHelperUITests/DJHelperUITests.swift
@@ -9,15 +9,8 @@
 import XCTest
 
 class DJHelperUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+    
+    let app = XCUIApplication()
 
     func testExample() {
         // UI tests must launch the application that they test.
