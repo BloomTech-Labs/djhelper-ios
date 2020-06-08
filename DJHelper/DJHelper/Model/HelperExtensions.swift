d0ced3962a0741d27c7a48b291c99175c653eb49
Michael <mrflowers22@uwalumni.com>
2020-06-08 11:52:14 -0700

    added extension on UIView to provide a shake animation

--- DJHelper/DJHelper/Model/HelperExtensions.swift
+++ DJHelper/DJHelper/Model/HelperExtensions.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 craigswanson. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension Date {
     func stringFromDate() -> String {
@@ -22,5 +22,18 @@ extension String {
         let formatter = DateFormatter()
         formatter.dateStyle = .short
         return formatter.date(from: self)
+    }
+}
+
+extension UIView {
+    func shake() {
+        let view = self
+        let propertyAnimator = UIViewPropertyAnimator(duration: 0.8, dampingRatio: 0.3) {
+            view.transform = CGAffineTransform(translationX: 15, y: 0)
+        }
+        propertyAnimator.addAnimations ({
+            view.transform = CGAffineTransform(translationX: 0, y: 0)
+        }, delayFactor: 0.4)
+        propertyAnimator.startAnimation()
     }
 }
