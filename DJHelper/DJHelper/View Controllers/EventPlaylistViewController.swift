d04dc6c35b570b134b58ff2b50feefcb73e6c3bf
Craig Swanson <cswanson02@me.com>
2020-06-08 21:37:11 -0500

    Added two files
    EventPlaylistVC and HostProfileVC were added.

---
+++ DJHelper/DJHelper/View Controllers/EventPlaylistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EventPlaylistViewController.swift
+//  DJHelper
+//
+//  Created by Craig Swanson on 6/8/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import UIKit
+
+class EventPlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
