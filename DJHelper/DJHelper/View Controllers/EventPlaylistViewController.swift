0595f90bc10a978880c8c028f1709ba61e837aff
Craig Swanson <cswanson02@me.com>
2020-06-29 09:38:28 -0500

    Added basic UI elements of the EventPlaylistViewController.swift file and wired them up.

--- DJHelper/DJHelper/View Controllers/EventPlaylistViewController.swift
+++ DJHelper/DJHelper/View Controllers/EventPlaylistViewController.swift
@@ -8,22 +8,70 @@
 
 import UIKit
 
-class EventPlaylistViewController: UIViewController {
+class EventPlaylistViewController: UIViewController, UISearchBarDelegate {
 
+    // MARK: - Properties
+    var event: Event?
+    var currentHost: Host?
+    private let refreshControl = UIRefreshControl()
+
+    // MARK: - Outlets
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var eventImage: UIImageView!
+    @IBOutlet var eventNameLabel: UILabel!
+    @IBOutlet var eventDescriptionLabel: UILabel!
+    @IBOutlet var hostNameButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+
+    // MARK: - View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        tableView.keyboardDismissMode = .onDrag
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refreshSongData(_:)), for: .valueChanged)
+
+        updateViews()
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    @objc func refreshSongData(_ sender: Any) {
+        updateSongList()
     }
-    */
+
+    func updateSongList() {
+        // call to the server for songs in Event
+        self.refreshControl.endRefreshing()
+    }
+    private func updateViews() {
+        guard let event = event,
+            let currentHost = currentHost else { return }
+
+        eventNameLabel.text = event.name
+        eventDescriptionLabel.text = event.eventDescription
+        hostNameButton.titleLabel?.text = currentHost.name
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // code to search for song
+    }
+}
+
+extension EventPlaylistViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
 
 }
