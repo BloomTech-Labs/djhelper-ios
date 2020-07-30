3f0df1c78249fedd3fe949ddc81656ade1c16ac5
Michael <mrflowers22@uwalumni.com>
2020-07-30 00:08:17 -0700

    created a HostEventViewCollectionViewCell

---
+++ DJHelper/DJHelper/HostEventCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HostEventCollectionViewCell.swift
+//  DJHelper
+//
+//  Created by Michael Flowers on 7/30/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import UIKit
+
+class HostEventCollectionViewCell: UICollectionViewCell {
+    
+}
