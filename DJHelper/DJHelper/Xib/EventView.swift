3599127253ef95668c93099c696c528f009f5e00
Michael <mrflowers22@uwalumni.com>
2020-07-29 23:50:14 -0700

    created an EventView.swift file that subclass UIView. Connected IBOutlets form the xib file to the EventView.swift file.

---
+++ DJHelper/DJHelper/Xib/EventView.swift
@@ -0,0 +1,25 @@
+//
+//  EventView.swift
+//  DJHelper
+//
+//  Created by Michael Flowers on 7/29/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import UIKit
+
+class EventView: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var eventNameLabel: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
