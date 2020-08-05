d3dbca068bb425e7cedc9dbb409a55fdd8763b28
Craig Swanson <cswanson02@me.com>
2020-08-05 16:59:31 -0500

    Created collection view cell layout for Upcoming Events.

---
+++ DJHelper/DJHelper/View Controllers/UpcomingEventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpcomingEventsViewController.swift
+//  DJHelper
+//
+//  Created by Craig Swanson on 8/5/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
