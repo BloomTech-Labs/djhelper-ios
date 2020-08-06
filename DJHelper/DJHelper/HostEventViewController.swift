c983840601a80c21d9b20f9f9dbbb9c952d5f265
Craig Swanson <cswanson02@me.com>
2020-08-05 21:14:31 -0500

    Replaced embedded collection views in HostEventCollectionVC with container views; added swift files for HostEventsVC and PastEventsVC.

--- DJHelper/DJHelper/HostEventViewController.swift
+++ DJHelper/DJHelper/HostEventViewController.swift
@@ -14,6 +14,7 @@ class HostEventViewController: UIViewController {
     // MARK: - Instance Variables
     var currentHost: Host?
     var eventController: EventController?
+    var hostController: HostController?
     let todaysDate = Date().stringFromDate()
     var eventsHappeningNow: [Event]?
     var upcomingEvents: [Event]?
@@ -26,9 +27,21 @@ class HostEventViewController: UIViewController {
     @IBOutlet weak var hostingEventCollectionView: UICollectionView!
     @IBOutlet weak var pastEventsCollectionView: UICollectionView!
 
+    // MARK: - View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchEventsFromServer()
+        let barViewControllers = self.tabBarController?.viewControllers
+        guard let profileVC = barViewControllers?[1] as? HostProfileViewController else { return }
+        profileVC.currentHost = self.currentHost
+        profileVC.hostController = self.hostController
+        guard let newEventNC = barViewControllers?[2] as? UINavigationController else { return }
+        if let newEventVC = newEventNC.viewControllers.first as? NewEventViewController {
+            newEventVC.eventController = self.eventController
+            newEventVC.hostController = self.hostController
+            newEventVC.currentHost = self.currentHost
+            newEventVC.hostEventCount = self.upcomingEvents?.count ?? 0
+        }
     }
 
     // MARK: - Private Methods
@@ -43,18 +56,11 @@ class HostEventViewController: UIViewController {
                   case .success:
                       DispatchQueue.main.async {
                         self.sortEvents(events: self.fetchRequest())
-                        self.setDataSourceForCollectionViews()
                       }
                   case .failure:
                       return
                   }
               }
-    }
-
-    private func setDataSourceForCollectionViews() {
-//        upcomingShowsCollectionView.dataSource = self
-        hostingEventCollectionView.dataSource = self
-        pastEventsCollectionView.dataSource = self
     }
 
         ///CODE FOR FETCHING FROM CORE DATA
@@ -85,59 +91,16 @@ class HostEventViewController: UIViewController {
         upcomingEventsVC.upcomingEvents = self.upcomingEvents
         print("upcomingEvents: \(String(describing: upcomingEvents?.count))")
     }
+    
+    // This action unwinds from the create new event tab after a new event is created.
+    @IBAction func unwindToEventList(segue: UIStoryboardSegue) {
+    }
 
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UpcomingEventsSegue" {
             guard let destinationVC = segue.destination as? UpcomingEventsViewController else { return }
             self.upcomingEventsVC = destinationVC
-        }
-    }
-}
-
-extension HostEventViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch collectionView {
-        case hostingEventCollectionView:
-            if let count = eventsHappeningNow?.count {
-                return count
-            } else {
-                return 1
-            }
-//        case upcomingShowsCollectionView:
-//            if let count = upcomingEvents?.count {
-//                return count
-//            } else {
-//                return 1
-//            }
-        case pastEventsCollectionView:
-            if let count = pastEvents?.count {
-                return count
-            }
-            return 1
-        default:
-            return 20
-        }
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        switch collectionView {
-        case hostingEventCollectionView:
-           let cell = hostingEventCollectionView.dequeueReusableCell(withReuseIdentifier: "hostingEventsCell",
-                                                                     for: indexPath) as! HostEventCollectionViewCell
-            return cell
-//        case upcomingShowsCollectionView:
-//            let cell = upcomingShowsCollectionView.dequeueReusableCell(withReuseIdentifier: "upcomingShowCell",
-//                                                                       for: indexPath) as! HostEventCollectionViewCell
-//            let event = upcomingEvents?[indexPath.row]
-//            cell.event = event
-//            return cell
-        case pastEventsCollectionView:
-            let cell = pastEventsCollectionView.dequeueReusableCell(withReuseIdentifier: "pastEventCell",
-                                                                    for: indexPath) as! HostEventCollectionViewCell
-            return cell
-        default:
-            return UICollectionViewCell()
         }
     }
 }
