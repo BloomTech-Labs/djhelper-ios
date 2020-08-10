025da67351dd03024debafb5c93165d8ef38c16e
Michael <mrflowers22@uwalumni.com>
2020-08-10 15:24:30 -0700

    edit the name of the playlist folder

---
+++ DJHelper/DJHelper/Model/Playlist Model/Playlist.swift
@@ -0,0 +1,13 @@
+//
+//  Playlist.swift
+//  DJHelper
+//
+//  Created by Michael Flowers on 8/10/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+import Foundation
+
+class Playlist {
+    
+}
